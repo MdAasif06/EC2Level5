9519331a9a73c7dd4b92af5635c4c39ceccef331
MdAasif06 <abdurm477@gmail.com>
2026-06-26 11:01:49 +0530

    second comment and understand promplem

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port=process.env.PORT||5000
 const app=express()
 
 app.get("/",(req,res)=>{
-    return res.status(200).json({message:"working home route"})
+    return res.status(200).json({message:"working home version 2"})
 })
 app.get("/hello",(req,res)=>{
-    return res.status(200).json({message:"All are Good👍"})
+    return res.status(200).json({message:"All are Good 👍"})
 })
 
 app.listen(port,()=>{
