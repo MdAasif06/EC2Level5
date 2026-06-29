dd1992bbb5f908c1eaa2a0befc07ffd6a3ab455e
MdAasif06 <abdurm477@gmail.com>
2026-06-29 16:09:13 +0530

    testing ci cd

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port=process.env.PORT||5000
 const app=express()
 
 app.get("/",(req,res)=>{
-    return res.status(200).json({message:"working home version 2.github"})
+    return res.status(200).json({message:"working home version-3"})
 })
 app.get("/hello",(req,res)=>{
-    return res.status(200).json({message:"All are Good 👍"})
+    return res.status(200).json({message:"All are Good ci cd working💖"})
 })
 
 app.listen(port,()=>{
