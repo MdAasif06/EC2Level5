63162ca96db22075f820904a4cab7005a21e6954
MdAasif06 <abdurm477@gmail.com>
2026-06-30 09:05:35 +0530

    cicd is working

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port=process.env.PORT||5000
 const app=express()
 
 app.get("/",(req,res)=>{
-    return res.status(200).json({message:" Home version-5"})
+    return res.status(200).json({message:" Home route version-6"})
 })
 app.get("/hello",(req,res)=>{
-    return res.status(200).json({message:"CI-CD working test from hello route"})
+    return res.status(200).json({message:"CI-CD working 👍"})
 })
 
 app.listen(port,()=>{
