5ad3220aee799b0024df10a4d4f5e3a516c7c8da
MdAasif06 <abdurm477@gmail.com>
2026-06-30 08:45:47 +0530

    something chnage and test

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port=process.env.PORT||5000
 const app=express()
 
 app.get("/",(req,res)=>{
-    return res.status(200).json({message:"working home version-4"})
+    return res.status(200).json({message:" Home version-5"})
 })
 app.get("/hello",(req,res)=>{
-    return res.status(200).json({message:"CI-CD working fine"})
+    return res.status(200).json({message:"CI-CD working test"})
 })
 
 app.listen(port,()=>{
